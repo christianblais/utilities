aab21fc95b2fad99694bc6bedab4b05426fbb16e
Christian Blais <christ.blais@gmail.com>
2011-08-21 17:04:58 -0400

    Add collect_keys & collect_values to Hash

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -280,9 +280,21 @@ module Utilities
 end
 
 class Hash
-  # Returns a new hash where {"a"=>1} becomes {:a=>1}
+  # Returns a new hash with the results of running block once for every key in self
+  def collect_keys
+    each_with_object({}){ |(k,v),h| h[yield(k)] = v } if block_given?
+  end
+  alias_method :map_keys, :collect_keys
+  
+  # Returns a new hash with the results of running block once for every value in self
+  def collect_values
+    each_with_object({}){ |(k,v),h| h[k] = yield(v) } if block_given?
+  end
+  alias_method :map_values, :collect_values
+  
+  # Returns a new hash where all keys have been symbolized
   def symbolize_keys
-    each_with_object({}){ |(k,v),h| h[k.to_sym] = v }
+    collect_keys{ |k| k.to_sym }
   end
 end
 
