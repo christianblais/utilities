445316813a4fffac3ddf6a9ed23b3518be9f2319
Guillaume Malette <gmalette@gmail.com>
2011-08-21 23:23:24 -0400

    Hello Exception, didnt't see you there. Thx Chris for the test ;).

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -343,14 +343,13 @@ module Enumerable
   # Collects the first N truthy values
   # {}.collect_first {|a, b| obj} => obj
   # {}.collect_first(n) {|a, b| obj} => [obj]
-  def collect_first( amount = nil, &block)
+  def collect_first( amount = nil, &block )
     array = !amount.nil?
     amount ||= 1
     
     values = []
-    enum = self.each
-    
-    while values.length < amount && (val = enum.next)
+    self.each do |val|
+      break if values.length >= amount
       t = yield(val)
       values << t if t
     end
