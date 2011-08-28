c5895b9fc385b7b83f5eeaa0ba021ef32d4047d9
Christian Blais <christ.blais@gmail.com>
2011-08-28 14:31:25 -0400

    Tests added for Array, Hash, Ranges and others

--- test/test_utilities.rb
+++ test/test_utilities.rb
@@ -1,20 +1,59 @@
+require "date"
 require File.join(File.dirname(__FILE__), "..", "lib", "utilities")
 
-describe Range do
-  it "#intersection should returns a range containing elements common to the two ranges, with no duplicates" do
-    (1..10).intersection(5..15).should == (5..10)
-    (1..10).intersection(15..25).should == nil
+describe Array do
+  it "#numerics? should returns true if given Array contains only numeric values" do
+    [].numerics?.should == true
+    [1,2,3,4,5].numerics?.should == true
+    [1,2,3,4,nil].numerics?.should == false
+    [1,2,3,4,nil].numerics?(true).should == true
   end
   
-  it "#empty? should returns true if given Range is empty" do
-    # Can a range be empty?
+  it "#to_numerics should returns a new array with only numeric elements" do
+    [].to_numerics.should == []
+    ["3", 4, nil].to_numerics.should == [3.0, 4.0, 0.0]
+    ["3", 4, nil].to_numerics(true).should == [3.0, 4.0, nil]
   end
   
-  it "#overlap? should returns true if the given Range overlaps the other" do
-    (1..5).overlap?(10..20).should == false
-    (1..5).overlap?(4..9).should == true
-    (1..5).overlap?(5..10).should == true
-    (1...5).overlap?(5..10).should == false
+  it "#reverse_sort should reverse sort the given array" do
+    [1,2,3,4].reverse_sort.should == [4,3,2,1]
+    [4,3,2,1].reverse_sort.should == [4,3,2,1]
+    ["f", "3", "z"].reverse_sort.should == ["z", "f", "3"]
+  end
+  
+  it "#to_stat should extends given array with module Utilities::Statistics" do
+    [].methods.include?(:statistics).should == false
+    [].to_stat.methods.include?(:statistics).should == true
+  end
+end
+
+describe Enumerable do
+  it "#collect_first should return the first collected elements" do
+    h = {:a => 1, :b => 2, :c => 3}
+    h.collect_first{|k, v| v if v == 2}.should == 2
+    h.collect_first(1){|k, v| v}.should == [1]
+    h.collect_first(2){|k, v| v}.should == [1, 2]
+  end
+end
+
+describe Hash do
+  it "#collect_keys should returns a new hash with the results of running block once for every key in self" do
+    {"a"=>1, "b"=>2, "c"=>3}.collect_keys{|k| k + "z"}.should == {"az"=>1, "bz"=>2, "cz"=>3}
+  end
+  
+  it "#collect_values should returns a new hash with the results of running block once for every value in self" do
+    {"a"=>1, "b"=>2, "c"=>3}.collect_values{|v| v**2}.should == {"a"=>1, "b"=>4, "c"=>9}
+  end
+  
+  it "#symbolize_keys should returns a new hash where all keys have been symbolized" do
+    {"a"=>1, "b"=>2, "c"=>3}.symbolize_keys.should == {:a=>1, :b=>2, :c=>3}
+  end
+end
+
+describe Kernel do
+  it "#raiser should raise given object.inspect" do
+    lambda{ raiser(1) }.should raise_error( RuntimeError, '1' )
+    lambda{ raiser([:a, :b], 3, "test") }.should raise_error( RuntimeError, '[:a, :b], 3, "test"' )
   end
 end
 
@@ -30,6 +69,12 @@ describe Numeric do
     (-5..5).each do |i|
       i.square.should == i**2
     end
+  end
+  
+  it "#hour_to_string should returns a formatted string of format HH:MM" do
+    14.5.hour_to_string.should == "14:30"
+    3.2.hour_to_string('h').should == "3h12"
+    19.hour_to_string.should == "19:00"
   end
   
   it "#sqrt should returns the square root of any given Numeric (based on Math::sqrt)" do
@@ -58,29 +103,25 @@ describe Numeric do
     1.percentage_of(50).should == 2
     1.percentage_of(1).should == 100
     1.percentage_of(50, 50).should == 1
+    18.percentage_of(20, 5).should == 4.5
     100.percentage_of(50).should == 200
   end
 end
 
-describe Hash do
-  it "#collect_keys should returns a new hash with the results of running block once for every key in self" do
-    {"a"=>1, "b"=>2, "c"=>3}.collect_keys{|k| k + "z"}.should == {"az"=>1, "bz"=>2, "cz"=>3}
+describe Range do
+  it "#intersection should returns a range containing elements common to the two ranges, with no duplicates" do
+    (1..10).intersection(5..15).should == (5..10)
+    (1..10).intersection(15..25).should == nil
   end
   
-  it "#collect_values should returns a new hash with the results of running block once for every value in self" do
-    {"a"=>1, "b"=>2, "c"=>3}.collect_values{|v| v**2}.should == {"a"=>1, "b"=>4, "c"=>9}
+  it "#empty? should returns true if given Range is empty" do
+    # Can a range be empty?
   end
   
-  it "#symbolize_keys should returns a new hash where all keys have been symbolized" do
-    {"a"=>1, "b"=>2, "c"=>3}.symbolize_keys.should == {:a=>1, :b=>2, :c=>3}
-  end
-end
-
-describe Enumerable do
-  it "#collect_first should return the first collected elements" do
-    h = {:a => 1, :b => 2, :c => 3}
-    h.collect_first{|k, v| v if v == 2}.should == 2
-    h.collect_first(1){|k, v| v}.should == [1]
-    h.collect_first(2){|k, v| v}.should == [1, 2]
+  it "#overlap? should returns true if the given Range overlaps the other" do
+    (1..5).overlap?(10..20).should == false
+    (1..5).overlap?(4..9).should == true
+    (1..5).overlap?(5..10).should == true
+    (1...5).overlap?(5..10).should == false
   end
 end
