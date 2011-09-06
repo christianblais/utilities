6a67c6e112eb41cf6375431b83f78382281d95d2
Christian Blais <christ.blais@gmail.com>
2011-09-06 12:00:03 -0400

    Added String#hour_to_float

--- lib/utilities/string.rb
+++ lib/utilities/string.rb
@@ -8,4 +8,10 @@ class String
   def to_time
     Time.parse(self)
   end
+
+  # Transform a string of format HH:MM into a float representing an hour
+  def hour_to_float(separator=':')
+    m, s = self.split(separator).map(&:to_f)
+    m + (s / 60)
+  end
 end
