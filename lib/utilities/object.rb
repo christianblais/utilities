0ef8367b16335f19ec0fac13a1272a47ea00b727
Christian Blais <christ.blais@gmail.com>
2011-10-11 13:59:10 -0400

    Add Object#attempt

--- lib/utilities/object.rb
+++ lib/utilities/object.rb
@@ -6,4 +6,13 @@ class Object
       enumerable.min <= self && self <= enumerable.max
     end
   end
+
+  # Attempts to call a method on given object. If it fails (nil or NoMethodError), returns nil
+  def attempt method, *args, &block
+    begin
+      self.try(method, *args, &block)
+    rescue NoMethodError
+      nil
+    end
+  end
 end
